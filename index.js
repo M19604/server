ef5a5e6ae737b36fbcc7e347bac692567f266695
M19604 <141754314+M19604@users.noreply.github.com>
2023-12-05 19:11:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+    console.log("New user connected.");
+    socket.on("disconnect", () => {
+        console.log("User disconnected.");
+    });
+    socket.on("chat-message", (msg) => {
+        console.log("Message: " + msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+});
